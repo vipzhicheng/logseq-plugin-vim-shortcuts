100a139e6c82d0152c14f2bb0757154c737bd836
Richard Yu <vipzhicheng@gmail.com>
2022-02-16 17:41:35 +0800

    fix: add github action

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,9 @@
 import { defineConfig } from "vite";
 
-import copy from "rollup-plugin-copy";
-
 export default defineConfig({
   base: "./",
   build: {
     target: "esnext",
     minify: "esbuild",
   },
-  plugins: [
-    copy({
-      targets: [
-        { src: "./icon.png", dest: "./dist/" },
-        { src: "./package.json", dest: "./dist/" },
-        { src: "./LICENSE", dest: "./dist/" },
-      ],
-      hook: 'writeBundle'
-    }),
-  ],
 });
